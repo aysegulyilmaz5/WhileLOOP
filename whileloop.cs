e08d785dcef2c3a81a63cf711c7d20530409fd70
aysegulyilmaz5 <yilmazansr@gmail.com>
2021-08-13 23:35:16 +0300

    Create whileloop.cs

---
+++ whileloop.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace whileloop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 1;
+            while (i <= 100)
+            {
+                listBox1.Items.Add("Hello World");
+                listBox2.Items.Add(i);
+                i++;
+            }
+
+            int j = 1;
+            int sum = 0;
+            while (j <= 100)
+            {
+                sum = sum + j;
+                j++;
+
+            }
+            MessageBox.Show(sum.ToString());
+        }
+    }
+}
